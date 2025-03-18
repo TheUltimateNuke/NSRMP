d7bb46fddb95704157ee09bea20ae59781b3de24
TheUltimateNuke <nuke.public@gmail.com>
2025-03-18 16:22:10 -0400

    Add project files

---
+++ NSRMP.Patcher/Patcher.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using Mirror.Weaver;
+using Mono.Cecil;
+
+namespace NSRMP.Patcher;
+
+public class BepInExLogger : Logger
+{
+    public void Warning(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Warning(string message, MemberReference mr)
+    {
+        Console.WriteLine(message, mr);
+    }
+
+    public void Error(string message)
+    {
+        Console.WriteLine($"Error: {message}");
+    }
+
+    public void Error(string message, MemberReference mr)
+    {
+        Console.WriteLine($"Error: {message} | {mr}");
+    }
+}
+
+public class Patcher
+{
+    public static IEnumerable<string> TargetDLLs { get; } = ["Assembly-CSharp.dll"];
+
+    public static void Patch(AssemblyDefinition assembly)
+    {
+        using (var asmResolver = new DefaultAssemblyResolver())
+        {
+            asmResolver.AddSearchDirectory(Paths.ManagedPath);
+            asmResolver.AddSearchDirectory(Path.Combine(Paths.PluginPath, "NSRMP", "Dependencies"));
+            asmResolver.AddSearchDirectory(Paths.BepInExAssemblyDirectory);
+
+            var weaver = new Weaver(new BepInExLogger());
+            Console.WriteLine(weaver.Weave(assembly, asmResolver, out var modified)
+                ? "Weaver complete!"
+                : "ERROR: Weaver failed!");
+
+            if (modified)
+            {
+                assembly.Write(new WriterParameters() { WriteSymbols = true });
+            }
+        }
+    }
+}
---
+++ NSRMP/Plugin.cs
@@ -0,0 +1,9 @@
+using BepInEx;
+
+namespace NSRMP;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    
+}
