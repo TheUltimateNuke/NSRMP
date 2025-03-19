7b011b35c4f249505ce16ccf174193c466457e41
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2025-03-18 21:03:15 -0400

    Start work on slime sync

--- NSRMP/Plugin.cs
+++ NSRMP/Plugin.cs
@@ -3,7 +3,10 @@ using BepInEx;
 using HarmonyLib;
 using kcp2k;
 using Mirror;
+using MonomiPark.SlimeRancher.Regions;
+using On.MonomiPark.SlimeRancher.DataModel;
 using UnityEngine;
+using XGamingRuntime;
 
 namespace NSRMP;
 
@@ -11,6 +14,7 @@ namespace NSRMP;
 public class Plugin : BaseUnityPlugin
 {
     public static GameObject? SingletonContainer { get; private set;  }
+    public static NetworkManager? NetworkManager { get; private set;  }
     
     private void Awake()
     {
@@ -23,7 +27,18 @@ public class Plugin : BaseUnityPlugin
         var networkManager = SingletonContainer.AddComponent<NetworkManager>();
         networkManager.dontDestroyOnLoad = true;
         SingletonContainer.AddComponent<NetworkManagerHUD>();
+        NetworkManager = networkManager;
         Debug.Log("NSRMP plugin loaded OK!");
+        
+        ActorModel.Init += ActorModelOnInit;
     }
 
+    private void ActorModelOnInit(ActorModel.orig_Init orig, MonomiPark.SlimeRancher.DataModel.ActorModel self, GameObject gameobj)
+    {
+        if (NetworkManager == null || !NetworkManager.isNetworkActive || !gameobj) return;
+        
+        var identity = gameobj.GetComponent<NetworkIdentity>() ?? gameobj.AddComponent<NetworkIdentity>();
+        var transform = gameobj.GetComponent<NetworkTransformUnreliable>() ?? gameobj.AddComponent<NetworkTransformUnreliable>();
+        // TODO If object has SlimeEmotions component, sync SlimeEmotions.currVal
+    }
 }
